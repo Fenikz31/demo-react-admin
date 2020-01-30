f84df59a3a08162259d55f9a06578c8d1cb2b17e
Patrick Kaninda <patrick.kaninda@interactiv-group.com>
2020-01-30 17:29:11 +0100

    Using a Custom Home Page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from 'react'
 import { Admin, Resource } from 'react-admin'
-import { UserList } from './components/users'
+
+import Dashboard from './components/dashboard'
 import {
   PostCreate,
   PostEdit,
   PostList
 } from './components/posts'
+import { UserList } from './components/users'
+
 import jsonServerProvider from 'ra-data-json-server'
 import PostIcon from '@material-ui/icons/Book'
 import UserIcon from '@material-ui/icons/Group'
+
 import logo from './logo.svg'
 import './App.css'
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.ir/');
 const App = () => (
-  <Admin dataProvider={dataProvider} >
+  <Admin dashboard={Dashboard} dataProvider={dataProvider} >
     <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon}/>
     <Resource name="users" list={UserList} icon={UserIcon} />
   </Admin>
