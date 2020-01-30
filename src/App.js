b4104ae2a1c48c5eb9c6c5a2366229dd12fc0684
Patrick Kaninda <patrick.kaninda@interactiv-group.com>
2020-01-30 15:07:21 +0100

    Mapping API Endpoints

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Admin, Resource } from 'react-admin'
+import { UserList } from './components/users'
+import jsonServerProvider from 'ra-data-json-server'
+import logo from './logo.svg'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const dataProvider = jsonServerProvider('https://jsonplaceholder.ir/');
+const App = () => (
+  <Admin dataProvider={ dataProvider } >
+    <Resource name="users" list={ UserList } />
+  </Admin>
+)
+
 
 export default App;
