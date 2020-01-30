fd3bf3de4234d0eea76034ec9df37a13a71a5b30
Patrick Kaninda <patrick.kaninda@interactiv-group.com>
2020-01-30 17:01:05 +0100

    Handling Relationshios

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import { Admin, Resource } from 'react-admin'
 import { UserList } from './components/users'
+import { PostList } from './components/posts'
 import jsonServerProvider from 'ra-data-json-server'
 import logo from './logo.svg'
 import './App.css'
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.ir/');
 const App = () => (
-  <Admin dataProvider={ dataProvider } >
-    <Resource name="users" list={ UserList } />
+  <Admin dataProvider={dataProvider} >
+    <Resource name="posts" list={PostList} />
+    <Resource name="users" list={UserList} />
   </Admin>
 )
 
